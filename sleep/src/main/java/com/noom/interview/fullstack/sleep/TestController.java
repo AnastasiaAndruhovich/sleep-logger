c04ac1930ae467fa4f8853882607b54de8140693
Anastasia Andruhovich <anastasia.andruhovich@gmail.com>
2024-11-26 16:25:47 +0100

    Init project

---
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/TestController.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 Noom, Inc.
+ */
+package com.noom.interview.fullstack.sleep;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello world!";
+    }
+}
---
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/db/DatabaseConfiguration.kt
@@ -0,0 +1,46 @@
+package com.noom.interview.fullstack.sleep.db;
+
+import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import java.sql.Connection;
+import javax.sql.DataSource;
+
+
+@Configuration
+@Profile("unit-test")
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("org.postgresql.Driver")
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+    @Bean
+    public Connection dbConnection(DataSource dataSource) throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
