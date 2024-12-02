2c0bcdbd740c3ce36d5de00b3c46eb5373d1a6db
Anastasia Andruhovich <anastasia.andruhovich@gmail.com>
2024-12-02 09:04:35 +0100

    #10 Turn off flyway on tests

--- sleep/src/test/java/com/noom/interview/fullstack/sleep/SleepApplicationTests.java
+++ sleep/src/test/java/com/noom/interview/fullstack/sleep/SleepApplicationTests.java
@@ -2,12 +2,11 @@ package com.noom.interview.fullstack.sleep;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class SleepApplicationTests {
 
     @Test
--- sleep/src/test/java/com/noom/interview/fullstack/sleep/repository/UserSleepRepositoryTest.java
+++ sleep/src/test/java/com/noom/interview/fullstack/sleep/repository/UserSleepRepositoryTest.java
@@ -11,9 +11,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @RequiredArgsConstructor
 public class UserSleepRepositoryTest {
 
